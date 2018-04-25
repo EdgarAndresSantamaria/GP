e193e0ac3a07ffe60be731f4761eb18adaeeec3d
Paula <pauladejaime@gmail.com>
2018-04-25 18:12:00 +0200

    ll

--- GP/001_implementacion/web/signup.php
+++ GP/001_implementacion/web/signup.php
@@ -1,12 +1,41 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Page Title</title>
+  <title>Sign Up</title>
+
+  <?php include 'parts/links.html';?>
+
 </head>
 <body>
 
-<h1>This is a Heading</h1>
-<p>This is a paragraph.</p>
+  <header>
+
+    <!-- Se anade el menu -->
+    <?php include 'parts/menu.php';?>
+
+  </header>
+
+  <div id="main-container">
+    <div id="left-panel">
+      imagen
+    </div>
+
+    <div id="right-panel">
+
+      <div class="formulario">
+        <form action="/my-handling-form-page" method="post">
+          <input type="text" id="username" placeholder="Username..." required/>
+          <input type="email" id="email" placeholder="Email..." required/>
+          <input type="password" id="password" placeholder="Password..." required/>
+          <button type="submit">Sign Up</button>
+        </br>
+      </br>
+      <span style="text-align: right">Already signed up? Log In!</span>
+    </form>
+  </div>
+
+
+</div>
 
 </body>
-</html> 
+</html>
