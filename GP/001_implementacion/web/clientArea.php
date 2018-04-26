c2f55237dc81e7acfd7db7e4e27df8c09508e922
edurne97 <36499960+edurne97@users.noreply.github.com>
2018-04-26 15:58:09 +0200

    home, clientArea

---
+++ GP/001_implementacion/web/clientArea.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Client Area</title>
+    <link rel="stylesheet" href="styles/styles.css">
+</head>
+
+<body>
+
+  <header>
+
+    <!-- Se anade el menu -->
+    <?php include 'parts/menu.php';?>
+
+  </header>
+
+</body>
+</html>
---
+++ GP/001_implementacion/web/home.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Home</title>
+    <link rel="stylesheet" href="styles/styles.css">
+</head>
+
+<body>
+
+  <header>
+
+    <!-- Se anade el menu -->
+    <?php include 'parts/menu.php';?>
+
+  </header>
+
+<div id="main">
+  <div id="left"
+    <div class="formularioHome">
+      <form action=" " method="post">
+        <span  style="position: absolute; top: 200px; left:200px;">SIGN UP AND PLAY!</span> </br/>
+        <span  style="position: absolute; top: 220px; left: 200px;">PLAY NOW!</span> </br/>
+        <span  style="position: absolute; top: 250px; left: 200px;">Escribir algo</span> </br/>
+        <button  style="position: absolute; top: 400px; left: 200px;" type="submit" >Sign up </button> </br/> </br/>
+        <button style="position: absolute; top: 450px; left: 200px;" type="submit" >Download </button>
+
+      </form>
+    </div>
+
+    <div id="right">
+    insertar foto
+  </div>
+</div>
+
+</body>
+</html>
