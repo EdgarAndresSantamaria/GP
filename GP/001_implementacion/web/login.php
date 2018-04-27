09ae6a60f4737ac78ae845f1785476216fc336a8
Paula <pauladejaime@gmail.com>
2018-04-27 18:39:24 +0200

    ll

--- GP/001_implementacion/web/login.php
+++ GP/001_implementacion/web/login.php
@@ -5,43 +5,13 @@
 session_start();
 ?>
 
+
 <!-- Si el usuario está identificado no se puede logear -->
 <?php
 if(isset($_SESSION["usuario"])){
   header("location: ../WEB");
 }
 ?>
-
-<!--Obtener datos formulario y guardarlos en mysql -->
-<?php
-if( isset($_POST["username"])) {
-  //se pone isset porque al principio no se le ha enviado nada al formulario y entonces sale error
-
-
-  //Se conecta con la base de datos
-  include("database/connect-database.php");
-
-  //Instrucción
-  $sql = "SELECT username, password FROM users WHERE username='". $_POST["username"] . "' AND password=MD5('" . $_POST["password"] . "')";
-  //Un resultado
-  $result = mysqli_query($conn, $sql);
-
-  if (mysqli_num_rows($result) > 0) {
-    //Se establece la variable de la sesión y se va a index.php
-    $_SESSION['usuario'] = $_POST["username"];
-    header("location: ../WEB");
-  } else {
-    //Si los datos son incorrectos
-    echo "<center><div style=\"color: red; font-size: 25px; background: black;\">-- Your username and password don't match --</div></center>";
-
-  }
-
-  //La conexión se cierra
-  mysqli_close($conn);
-
-}
-?>
-<!-- Fin -->
 
 <html>
 <head>
@@ -67,6 +37,44 @@ if( isset($_POST["username"])) {
     <div id="right-panel">
 
       <div class="formulario">
+
+
+        <!--Obtener datos formulario y guardarlos en mysql -->
+        <?php
+        if( isset($_POST["username"])) {
+          //se pone isset porque al principio no se le ha enviado nada al formulario y entonces sale error
+
+
+          //Se conecta con la base de datos
+          include("database/connect-database.php");
+
+          //Instrucción
+          $sql = "SELECT username, password FROM users WHERE username='". $_POST["username"] . "' AND password=MD5('" . $_POST["password"] . "')";
+          //Un resultado
+          $result = mysqli_query($conn, $sql);
+
+          if (mysqli_num_rows($result) > 0) {
+            //Se establece la variable de la sesión y se va a index.php
+            $_SESSION['usuario'] = $_POST["username"];
+            header("location: ../WEB");
+          } else {
+            //Si los datos son incorrectos
+            echo "<center><div style=\"color: #060606;
+            font-size: 15px;
+            margin-bottom: 14px;
+            border-bottom: 3px solid #454545;
+            border-top: 3px solid #454545;
+            padding: 5px;\">-- Your username and password don't match --</div></center>";
+          }
+
+          //La conexión se cierra
+          mysqli_close($conn);
+
+        }
+        ?>
+        <!-- Fin -->
+
+
         <form action="<?php $_PHP_SELF ?>" method="post">
           <input type="text" id="username" name="username" placeholder="Username..." required/>
           <input type="password" id="password" name="password" placeholder="Password..." required/>
