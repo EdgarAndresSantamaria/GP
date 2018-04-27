bbdd200378ddac97a366331c8234a5e9aa474a92
Paula <pauladejaime@gmail.com>
2018-04-27 17:03:23 +0200

    asdf

--- GP/001_implementacion/web/home.php
+++ GP/001_implementacion/web/home.php
@@ -14,13 +14,13 @@
 
   </header>
 
-<div id="main">
+<div id="main-container">
   <div id="left"
     <div class="formularioHome">
       <form action=" " method="post">
-        <span  style="position: absolute; top: 200px; left:200px;">SIGN UP AND PLAY!</span> </br/>
-        <span  style="position: absolute; top: 220px; left: 200px;">PLAY NOW!</span> </br/>
-        <span  style="position: absolute; top: 250px; left: 200px;">Escribir algo</span> </br/>
+        SIGN UP AND PLAY!</br/>
+        PLAY NOW!</br/>
+        Escribir algo</br/>
         <button  style="position: absolute; top: 400px; left: 200px;" type="submit" >Sign up </button> </br/> </br/>
         <button style="position: absolute; top: 450px; left: 200px;" type="submit" >Download </button>
 
--- GP/001_implementacion/web/parts/menu.php
+++ GP/001_implementacion/web/parts/menu.php
@@ -1,9 +1,10 @@
+
 <div id="menu">
   <div class="topnav" id="myTopnav">
-   <a href="#home" class="active">Home</a>
-   <a href="#news">Log In</a>
-   <a href="#contact">Sign Up</a>
-   <a href="#about">Client Area</a>
-   <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
- </div>
+    <a href="home.php" class="active">Home</a>
+    <a href="login.php">Log In</a>
+    <a href="signup.php">Sign Up</a>
+    <a href="clientArea.php">Client Area</a>
+    <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
+  </div>
 </div>
