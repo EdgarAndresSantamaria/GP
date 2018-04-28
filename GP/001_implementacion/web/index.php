43d4ae2a73d76d79689c829f6246c0cd9b67ef0a
Paula <pauladejaime@gmail.com>
2018-04-28 18:12:06 +0200

    Update clientArea.php, index.php, and 4 more files...

--- GP/001_implementacion/web/index.php
+++ GP/001_implementacion/web/index.php
@@ -6,8 +6,8 @@ session_start();
 
 <html>
 <head>
-<title>Home</title>
-    <?php include 'parts/links.html';?>
+  <title>Home</title>
+  <?php include 'parts/links.html';?>
 </head>
 
 <body>
@@ -19,23 +19,39 @@ session_start();
 
   </header>
 
-<div id="main-container">
-  <div id="left"
-    <div class="formularioHome">
-      <form action=" " method="post">
-        SIGN UP AND PLAY!</br/>
-        PLAY NOW!</br/>
-        Escribir algo</br/>
-        <button type="submit" >Sign up </button> </br/> </br/>
-        <button type="submit" >Download </button>
+  <div id="main-container">
+    <div id="left">
+    <div class="containerHome">
+      <!-- Si el usuario no está identificado -->
+      <?php
+      if(!isset($_SESSION['usuario'])){
+        ?>
+        <h2>SIGN UP AND PLAY!</h2>
+        <img src="images/pong.png"/>
+        <br />
+        <br />
+        <button onclick="window.location.href='signup.php'">Sign up </button>
+        <button onclick="window.location.href='login.php'">Log In </button>
 
-      </form>
+        <?php
+      }else{
+        ?>
+          <h2>PLAY NOW!</h2></br/>
+        <img src="images/pong.png"/>
+        <br />
+        <br />
+        <button type="window.location.href='linkDescargaDirecta'" >Download </button>
+
+        <?php
+      }
+      ?>
     </div>
+  </div>
 
-    <div id="right">
+  <div id="right">
 
   </div>
 </div>
-  <?php include 'parts/creditos.html';?>
+<?php include 'parts/creditos.html';?>
 </body>
 </html>
