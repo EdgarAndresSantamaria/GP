d662cc7e657a506efd121fc99c420f94ac7a1a02
Paula <pauladejaime@gmail.com>
2018-05-04 16:40:15 +0200

    .

--- GP/001_implementacion/PONG/src/packModelo/GestorBD.java
+++ GP/001_implementacion/PONG/src/packModelo/GestorBD.java
@@ -7,17 +7,16 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
+import packExcepciones.ExcepcionDatoActualizado;
+import packExcepciones.ExcepcionDatoNoActualizado;
+import packGestores.GestorBD;
+
 public class GestorBD {
 	private static GestorBD miGestorBD;
-	private ResultSet resultado;
-	private Connection connection;
-	private Statement instruccion;
-	private String bd = "auditingAB";
+	private Connection myConn;
+	private Statement myStmt;
 
-	public String getBD(){
-		return bd;
-	}
-	
+
 	/**
 	 * constructora
 	 */
@@ -43,24 +42,37 @@ public class GestorBD {
 	 * @param user
 	 * @param password
 	 */
-	public void OpenConnection() throws SQLException {
-			connection= DriverManager.getConnection("jdbc:mysql://galan.ehu.eus:3306/Xpdejaime001_pong", "Xpdejaime001", "AIC7nDBJ");
-			instruccion = connection.createStatement();
+	public void conectar() throws SQLException {
+
+		// 1. Get a connection to database
+		myConn = DriverManager.getConnection("jdbc:mysql://galan.ehu.eus:3306/Xpdejaime001_pong", "Xpdejaime001", "AIC7nDBJ");
+
+		System.out.println("Database connection successful!\n");
+
+		// 2. Create a statement
+		myStmt = myConn.createStatement();
 	}
 
 	/**
 	 * metodo para cerrar la actual conexion hacia la BD
 	 */
-	public void CloseConnection()  throws SQLException  {	
-			connection.close();
+	public void cerrarConexion()  throws SQLException  {	
+		if (myStmt != null) {
+			myStmt.close();
+		}
+
+		if (myConn != null) {
+			myConn.close();
+		}
 	}
 
 	/**
 	 * metodo update que ejecuta sentencias SQL del tipo update,insert,delete
 	 * @param SentenciaSQL
 	 */
-	private void Update(String SentenciaSQL)  throws SQLException  {
-			instruccion.executeUpdate(SentenciaSQL);
+	private void exeSQLUpdate(String SentenciaSQL)  throws SQLException  {
+		myStmt.executeUpdate(SentenciaSQL);
+
 	}
 
 	/**
@@ -70,23 +82,43 @@ public class GestorBD {
 	 * @return
 	 * @throws SQLException
 	 */
-	private ResultSet Select(String SentenciaSQL) throws SQLException {
-		resultado = instruccion.executeQuery(SentenciaSQL);
-		return resultado;
+	private ResultSet exeSQLSelect(String SentenciaSQL) throws SQLException {
+		ResultSet myRs = null;
+		// 3. Execute SQL query
+		myRs = myStmt.executeQuery(SentenciaSQL);
+		return myRs;
 	}
-	
+
 	public ArrayList<Rank> cargar(String pJugador1) {
 		//cargar el ranking del jugador especificado
 		return null;
 	}
-	
+
 	public void guardar(ArrayList<Rank> ranking) {
 		//guardar el ranking del jugador especificado
 	}
 
 	public Boolean existeUsuario(String nombre, String pwd) {
-		return null;
-		// TODO Auto-generated method stub
-		
+		Boolean encontrado = false;
+		try {
+			// Se conecta
+			conectar();
+
+			// Se pide
+			ResultSet myRs = exeSQLSelect("SELECT * FROM users WHERE username='" + nombre + "' AND password='"+ pwd + "'");
+
+			// Se procesa el resultado
+			encontrado = myRs != null && myRs.next();
+
+			// Se cierra
+			cerrarConexion();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return encontrado;
+
 	}
 }
