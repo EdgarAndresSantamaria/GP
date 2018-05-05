b52d6853a1fe6073f52f13a9aca65da3474ffff5
Ana Miranda <36272243+anitamiranda27@users.noreply.github.com>
2018-05-05 12:10:42 +0200

    ...

--- GP/001_implementacion/PONG/src/packMain/Main.java
+++ GP/001_implementacion/PONG/src/packMain/Main.java
@@ -1,10 +1,14 @@
 package packMain;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import packVista.F00Inicio;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+		UIManager.setLookAndFeel("com.jtattoo.plaf.acryl.AcrylLookAndFeel");
 		F00Inicio inicio = new F00Inicio();
 		//F04MenuJuego m = new F04MenuJuego(false);
 	}
